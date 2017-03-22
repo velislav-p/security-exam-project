2780263b7609f4fc169cd307319a263507b24696
AndersBlack <andblack92@gmail.com>
2017-03-22 15:01:32 +0100

    css changes
    BLUE AND ORANGE BITCH !

--- views/passwordRecovery.php
+++ views/passwordRecovery.php
@@ -6,15 +6,22 @@ $key = $_GET["key"];
 <head>
     <meta charset="UTF-8">
     <title>Password recovery</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <!-- StyleSheet -->
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
 
-<div id="wdw-password-recovery">
-    <form method="post" action="../services/passwordRecovery.php">
-        <input type="text" name="username">
-        <input type="text" name="password">
+<div id="wdw-password-recovery" class="form-group">
+    <form method="post" class="input-form" action="../services/passwordRecovery.php">
+        <input type="text" class="form-control" name="username">
+        <input type="text" class="form-control" name="password">
         <input type="hidden" name="key" value="<?php echo $key ?>">
-        <input type="submit" value="Recover password">
+        <input type="submit" class="btn btn-warning" value="Recover password">
     </form>
 </div>
 </body>
