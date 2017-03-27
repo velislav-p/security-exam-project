7ac337f49f55ce2a4c32dfa392426a0d09fc2645
Velislav <v.peychev@yahoo.com>
2017-03-27 12:57:23 +0200

    connection and sql table

--- services/requestPassword.php
+++ services/requestPassword.php
@@ -2,9 +2,16 @@
 
 require 'connection.php';
 
+
 $email = $_POST["email"];
 $key = "secretkey";
-$link = "http://v-peychev.dk/SecuritySemesterProject/views/passwordRecovery.php?key=".$key;
+$link = 'example';
+
+$stmt = $db -> prepare("SELECT * FROM user WHERE username = :username");
+$stmt -> bindValue(":username",$username);
+$stmt->execute();
+$rows = $stmt->fetch(PDO::FETCH_ASSOC);
+
 
 //prepare statement to check the database for email address and create secret key that identifies the account;
 //if yes, send this email, else send an email saying the email is not associated to an account.
