0fe6f6697aa8f23a61f494405a2175ac1127d1d2
AndersBlack <andblack92@gmail.com>
2017-04-25 11:19:46 +0200

    profile/logout changes, functions removal

--- views/profile.php
+++ views/profile.php
@@ -1,15 +1,15 @@
 <?php
   session_start();
+  // error_reporting(0);
 
-
-  if(isset($_SESSION['username']))  {
+  if(!empty($_SESSION['username']))  {
 
     $username = base64_decode($_SESSION['username']);
 
   }else{
     session_destroy();
 
-    header("index.html");
+    // header("Location: ../index.html");
   }
 
 ?>
@@ -19,8 +19,22 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome to your profile!</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <!-- StyleSheet -->
+    <link rel="stylesheet" href="../style/style.css">
   </head>
   <body>
-    <h2>HALLOOOOOOO AND WELCOME <?php print $username; ?></h2>
+
+    <div class="wdw-profile-form form-group">
+      <h4>Welcome to Chatter <?php echo $username ?></h4>
+      <a type="button" class="btn btn-warning" name="button">Start a new room</a>
+      <a type="button" class="btn btn-warning" name="button">Change your profile</a>
+      <a type="button" href="../services/logout.php" class="btn btn-danger" name="button">Logout</a>
+    </div>
+
   </body>
 </html>
